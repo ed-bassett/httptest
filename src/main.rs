ac9f7dff7cd92b90dd677b097760a2865d633df9
Ed Bassett <ed@devsketchpad.com>
2025-05-04 20:54:57 +1000

    Update to hyper 1.6.0

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,47 @@
 use std::{
   fs::File,
-  io::Write,
+  io::Write, net::SocketAddr,
 };
 
 use futures::StreamExt;
-use hyper::{
-  Request, Response, Method, StatusCode,
-  service::{service_fn, make_service_fn}, Body, Server,
-};
 use tracing::{info, trace, error};
+
+use http_body_util::{BodyExt, Full};
+use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response, Method, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
   let subscriber = tracing_subscriber::fmt().finish();
   tracing::subscriber::set_global_default(subscriber)?;
 
-  let addr = ([127, 0, 0, 1], 3222).into();
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3222));
 
-  let server = Server::bind(&addr).serve(make_service_fn(|_conn|
-    async { Ok::<_, hyper::http::Error>(service_fn(image)) }
-  ));
+  let listener = TcpListener::bind(addr).await?;
 
   info!("Starting server at: {:?}", addr);
 
-  server.await?;
+  loop {
+    let (stream, _) = listener.accept().await?;
 
-  Ok(())
+    // Use an adapter to access something implementing `tokio::io` traits as if they implement
+    // `hyper::rt` IO traits.
+    let io = TokioIo::new(stream);
+
+    // Spawn a tokio task to serve multiple connections concurrently
+    tokio::task::spawn(async move {
+      if let Err(err) = http1::Builder::new()
+        .serve_connection(io, service_fn(image))
+        .await
+      {
+        eprintln!("Error serving connection: {:?}", err);
+      }
+    });
+  }
 }
 
-async fn image(req: Request<Body>) ->  Result<Response<Body>, hyper::http::Error>  {
+async fn image(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
   match (req.method(), req.uri().path()) {
     (&Method::PUT, "/") => {
       info!("Received PUT request with content_length: {:?}", req.headers().get(hyper::header::CONTENT_LENGTH).unwrap());
@@ -36,10 +49,10 @@ async fn image(req: Request<Body>) ->  Result<Response<Body>, hyper::http::Error
       let Ok(file) = File::create("./output.png") else {
         error!("Couldn't open file for write");
 
-        return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from("Couldn't open file for write"));
+        return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Full::new(Bytes::from("Couldn't open file for write")));
       };
 
-      match req.into_body().scan((file, false), |(file, ref mut was_err), chunk| {
+      match req.into_body().into_data_stream().scan((file, false), |(file, ref mut was_err), chunk| {
         if *was_err { return futures::future::ready(None) }
 
         let result = (|| {
@@ -53,13 +66,13 @@ async fn image(req: Request<Body>) ->  Result<Response<Body>, hyper::http::Error
 
         futures::future::ready(Some(result))
       }).collect::<Vec<_>>().await.into_iter().collect::<anyhow::Result<Vec<_>>>() {
-        Ok(_)  => Response::builder().status(StatusCode::OK).body(Body::empty()),
+        Ok(_)  => Response::builder().status(StatusCode::OK).body(Full::new(Bytes::new())),
         Err(_) => {
           error!("Error writing to file");
-          Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from("Error writing to file"))
+          Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Full::new(Bytes::from("Error writing to file")))
         },
       }
     },
-    _ => Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty())
+    _ => Response::builder().status(StatusCode::NOT_FOUND).body(Full::new(Bytes::new()))
   }
 }
